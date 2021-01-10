2f3c33ad295c82a3ddd9efe05d0c0629c54cbbd3
joaowilian01 <joaowilian01@gmail.com>
2021-01-10 15:50:16 -0300

    Get hours, minutes and seconds from Date object

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const clockContainer = document.querySelector('.clock-container')
 
 const updateClock = () => {
   const present = new Date()
-
-  console.log(present)
+  const hours = present.getHours()
+  const minutes = present.getMinutes()
+  const seconds = present.getSeconds()
+  
+  console.log(hours, minutes, seconds)
 }
 
 setInterval(updateClock, 1000);
